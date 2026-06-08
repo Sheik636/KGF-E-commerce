3368c8c6c031885fbc89b7b37183322850e66d07
sheik ahamed <sheikmusthak006@gmail.com>
2026-06-08 16:05:05 +0530

    UI improvements

--- client/src/Pages/MyOrders.jsx
+++ client/src/Pages/MyOrders.jsx
@@ -62,36 +62,80 @@ const MyOrders =()=>{
             {loading?(
                 <h2>Loading...</h2>
             ):orders.length==0?(
-                <h2>No Orders Found</h2>
-            ):(orders.map((order)=>(
-                <div key={order._id} className="border rounded-lg p-4 mb-6 shadow">
-                    <div className="flex justify-between mb-4">
-                        <div>
-                            <h2 className="font-bold">Order ID:</h2>
-                            <p>{order._id}</p>
-                        </div>
-                    <div>
-                        <h2 className="font-bold">Total:</h2>
-                        <p>₹{order.totalPrice}</p>
-                    </div>
+                <div className="text-center mt-20">
+                    <h2 className="text-3xl mb-4">
+                        📦
+                    </h2>
+                    <h2 className="text-xl font-semibold">
+                        No Orders Yet
+                    </h2>
+                    <p className="text-gray-500 mt-2">
+                        Start shopping to place your first order
+                    </p>
                 </div>
-                {order.orderItems.map((item)=>(
-                    <div key={item._id} className="flex items-center gap-4 border-b py-3 ">
-                        <img src={item.image} alt={item.name} className="w-20 h-20 object-cover rounded" />
-                        <div>
-                            <h3 className="font-semibold">{item.name}</h3>
-                            <p>Qty:{item.quantity}</p>
-                            <p>₹{item.price}</p>
+            ):(orders.map((order)=>(
+                <div key={order._id} className="bg-white rounded-3xl p-6 mb-8 shadow-xl border border-gray-100 hover:shadow-2xl transition-all duration-300">
+                    <div className="flex flex-col md:flex-row justify-between mb-6 gap-4">
+                        <div className="bg-gradient-to-r from-red-500 to-blue-500 text-white p-4 rounded-2xl mb-5">
+                            <h2 className="text-xl font-bold">
+                                 Order #{order._id.slice(-6)}
+                            </h2>
                         </div>
-                    </div>
-                ))}
+
+                        <div>
+                            <h2 className="font-bold text-gray-700">
+                            Order Date
+                            </h2>
+                            <p>
+                            {new Date(order.createdAt)
+                                .toLocaleDateString()}
+                            </p>
+                        </div>
+
+                        <div>
+                            <h2 className="font-bold text-gray-700">
+                            Total
+                            </h2>
+                            <p className="text-green-600 font-bold">
+                            ₹{order.totalPrice}
+                            </p>
+                        </div>
+                        </div>
+                {order.orderItems.map((item) => (
+                            <div
+                                key={item._id}
+                                className="flex gap-5 border rounded-xl p-4 mb-4 bg-white shadow-sm hover:shadow-md transition item-center"
+                            >
+                                <img
+                                src={item.image}
+                                alt={item.name}
+                                className="w-28 h-28 rounded-xl object-cover shadow-md"
+                                />
+                                <div className="flex-1">
+                                <h3 className="text-xl font-bold text-gray-800">
+                                    {item.name}
+                                </h3>
+                                <p className="text-gray-500">
+                                    Qty: {item.quantity}
+                                </p>
+                                {item.size && (
+                                    <p className="text-gray-500">
+                                    Size: {item.size}
+                                    </p>
+                                )}
+                                <p className="font-bold text-green-600">
+                                    ₹{item.price}
+                                </p>
+                                </div>
+                            </div>
+                            ))}
                 <div className="mt-4 flex gap-4">
                     <span className={`px-3 py-1 rounded text-white ${order.isPaid ? "bg-green-500": "bg-red-500"}`}>{order.isPaid ? "Paid" : "Not Paid"}</span>
                     <span className={`px-3 py-1 rounded text-white
                   ${order.isDelivered ? "bg-green-500" : "bg-yellow-500"}`}>{order.isDelivered ? "Delivered" : "Pending"}</span>
                 </div>
                 {!order.isPaid && (
-                            <button onClick={()=> payment(order)} className="bg-green-500 text-white px-4 py-2 rounded mt-4">Pay now</button>
+                            <button onClick={()=> payment(order)} className="mt-5 bg-gradient-to-r from-green-500 to-emerald-600 text-white px-6 py-3 rounded-xl font-semibold shadow-lg hover:scale-105 transition">Pay now</button>
                 )}
             </div>
             ))
